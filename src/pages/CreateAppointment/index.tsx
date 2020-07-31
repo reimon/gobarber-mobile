dbc05bcfbae861a38021187e4ea4af5ad1004b62
Reimon Ferreira <reimonf@gmail.com>
2020-07-31 11:15:19 -0300

    :rocket: Estrutura de criação

--- src/pages/CreateAppointment/index.tsx
+++ src/pages/CreateAppointment/index.tsx
@@ -1,14 +1,35 @@
-import React from 'react';
+import React, {useCallback} from 'react';
+import {useRoute} from '@react-navigation/native';
+import {useNavigation} from '@react-navigation/native';
+import Icon from 'react-native-vector-icons/Feather';
 
-import {Container, Button} from './styles';
-import {View, Text} from 'react-native';
+import {Container, Header, BackButton, HeaderTitle, UserAvatar} from './styles';
+import {useAuth} from '../../hooks/auth';
+
+interface RouteParms {
+  providerId: string;
+}
 
 const CreateAppointment: React.FC = () => {
+  const {user} = useAuth();
+  const route = useRoute();
+  const providerId = route.params as RouteParms;
+  const {goBack} = useNavigation();
+
+  const navigateBack = useCallback(() => {
+    goBack();
+  }, [goBack]);
+
   return (
     <Container>
-      <View>
-        <Text>CreateAppointment</Text>
-      </View>
+      <Header>
+        <BackButton onPress={navigateBack}>
+          <Icon name="chevron-left" size={24} color="#999591" />
+        </BackButton>
+        <HeaderTitle>Cabeleireiro</HeaderTitle>
+
+        <UserAvatar source={{uri: user.avatar_url}} />
+      </Header>
     </Container>
   );
 };
