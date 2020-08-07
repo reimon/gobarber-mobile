abc8ecd313d220b8f47eab6b4f1d1515e652269d
Reimon Ferreira <reimonf@gmail.com>
2020-08-07 19:45:27 -0300

    Mostrando horários em tela

--- src/pages/CreateAppointment/styles.ts
+++ src/pages/CreateAppointment/styles.ts
@@ -1,14 +1,21 @@
 import styled from 'styled-components/native';
 import {getStatusBarHeight} from 'react-native-iphone-x-helper';
 import {FlatList} from 'react-native';
-import {Provider} from './index';
 import {RectButton} from 'react-native-gesture-handler';
+import {Provider} from './index';
 
 interface ProviderContainerProps {
   selected: boolean;
 }
-
 interface ProviderNameProps {
+  selected: boolean;
+}
+
+interface HourProps {
+  available: boolean;
+  selected: boolean;
+}
+interface HourTextProps {
   selected: boolean;
 }
 
@@ -20,16 +27,16 @@ export const Header = styled.View`
   padding: 24px;
   padding-top: ${getStatusBarHeight() + 24}px;
   background: #28262e;
-
   flex-direction: row;
   justify-content: space-between;
   align-items: center;
 `;
+
 export const BackButton = styled.TouchableOpacity``;
 
 export const HeaderTitle = styled.Text`
   color: #f5ede8;
-  font-family: 'Robotoslab-Medium';
+  font-family: 'RobotoSlab-Medium';
   font-size: 20px;
   margin-left: 16px;
 `;
@@ -45,6 +52,8 @@ export const ProvidersListContainer = styled.View`
   height: 112px;
 `;
 
+export const Content = styled.ScrollView``;
+
 export const ProvidersList = styled(FlatList as new () => FlatList<Provider>)`
   padding: 32px 24px;
 `;
@@ -57,11 +66,13 @@ export const ProviderContainer = styled(RectButton)<ProviderContainerProps>`
   margin-right: 16px;
   border-radius: 10px;
 `;
+
 export const ProviderAvatar = styled.Image`
   width: 32px;
   height: 32px;
   border-radius: 16px;
 `;
+
 export const ProviderName = styled.Text<ProviderNameProps>`
   margin-left: 8px;
   font-family: 'RobotoSlab-Medium';
@@ -91,4 +102,39 @@ export const OpenDatePickerButtonText = styled.Text`
   font-family: 'RobotoSlab-Medium';
   font-size: 16px;
   color: #232129;
+`;
+
+export const Schedule = styled.View`
+  padding: 24px 0 16px;
+`;
+
+export const Section = styled.View`
+  margin-bottom: 24px;
+`;
+
+export const SectionTitle = styled.Text`
+  font-size: 18px;
+  color: #999591;
+  font-family: 'RobotoSlab-Regular';
+  margin: 0 24px 12px;
+`;
+
+export const SectionContent = styled.ScrollView.attrs({
+  contentContainerStyle: {paddingHorizontal: 24},
+  horizontal: true,
+  showsHorizontalScrollIndicator: false,
+})``;
+
+export const Hour = styled(RectButton)<HourProps>`
+  padding: 12px;
+  background: ${(props) => (props.selected ? '#ff9000' : '#3e3b47')};
+  border-radius: 10px;
+  margin-right: 8px;
+  opacity: ${(props) => (props.available ? 1 : 0.3)};
+`;
+
+export const HourText = styled.Text<HourTextProps>`
+  color: ${(props) => (props.selected ? '#232129' : '#f4ede8')};
+  font-family: 'RobotoSlab-Regular';
+  font-size: 16px;
 `;
--- src/pages/Profile/index.tsx
+++ src/pages/Profile/index.tsx
@@ -1,15 +1,18 @@
 import React, {useCallback} from 'react';
 import {useNavigation} from '@react-navigation/native';
 import {Container, Button, Header} from './styles';
+import {useAuth} from '../../hooks/auth';
 
 const Profile: React.FC = () => {
+  const {signOut} = useAuth();
+
   const {navigate} = useNavigation();
   const navigateToDashboard = useCallback(() => {
     navigate('Dashboard');
   }, [navigate]);
   return (
     <Container>
-      <Button title="Dashboard" onPress={navigateToDashboard} />
+      <Button title="Logout" onPress={signOut} />
     </Container>
   );
 };
